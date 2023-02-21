b8485c2122e92187beb1c0f7bb9d615bd048f294
SunilKharsu <112311905+SunilKharsu@users.noreply.github.com>
2023-02-21 11:41:32 +0530

    Add files via upload

---
+++ AddInteger.java
@@ -0,0 +1,11 @@
+class AddInteger{
+    public static void main(String []args){
+        System.out.println("Enter two numbers");
+        int first = 10;
+        int second = 20;
+        System.out.println(first+" "+second);
+
+        int sum = first + second;
+        System.out.println("This sum is:"+sum);
+    }
+}
